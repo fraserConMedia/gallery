e8488fa7ed5469c804a73c1d4bbb3f878b99a014
fraser <fraserconstructionmedia@gmail.com>
2020-05-30 10:51:17 -1000

    Added kitchen

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,7 @@ import React from "react";
 import Gallery from "react-photo-gallery";
 import {photos} from './photos'
 import { outdoorPhotos } from "./outdoorPhotos";
+import {kitchenPhotos} from "./kitchenPhotos"
 import { SRLWrapper } from "simple-react-lightbox";
 import './App.css'
 
@@ -15,11 +16,18 @@ function PhotoGallery() {
     <div>
     <SRLWrapper>
       <div className='photo-container'>
-      <Gallery className="photo" photos={photos} />
-      <div className="break"> </div>
-      <Gallery className="photo" photos={outdoorPhotos} />
-      </div>
+      <Gallery className="photo" photos={kitchenPhotos} />
+
+      
+        <Gallery className="photo" photos={photos} />
+      
+
+      
+         <Gallery className="photo" photos={outdoorPhotos} />
+     
+</div>
     </SRLWrapper>
+  
     <footer class="py-5 bg-dark">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; Fraser Construction 2020</p>
---
+++ src/kitchenPhotos.js
@@ -0,0 +1,193 @@
+export const kitchenPhotos = [
+    {
+        src: "/img/kitchen/1.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/2.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/3.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/4.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/5.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/6.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/7.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/8.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/9.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/10.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/11.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/12.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/13.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/14.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/15.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/16.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/17.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/18.jpg",
+        width: 4,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/19.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/20.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/21.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/22.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/23.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/24.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/25.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/26.jpg",
+        width: 4,
+        height: 4
+      },
+
+
+      {
+        src: "/img/kitchen/27.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/28.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/29.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/30.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/31.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/32.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/33.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/34.jpg",
+        width: 4,
+        height: 4
+      },
+
+      {
+        src: "/img/kitchen/35.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "/img/kitchen/36.jpg",
+        width: 4,
+        height: 4
+      },
+
+      
+]
